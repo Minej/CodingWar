b9bf884f43d7b7cca29c807e653016373d69d14d
Jamil <vjagaev24@gmail.com>
2021-01-17 01:30:08 +0400

    adding View_friend_activity

--- app/src/main/java/com/example/myapp5/ViewFriendActivity.java
+++ app/src/main/java/com/example/myapp5/ViewFriendActivity.java
@@ -1,11 +1,33 @@
 package com.example.myapp5;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ViewFriendActivity extends AppCompatActivity {
+
+    DatabaseReference mUserRef;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    String profileImageUrl, username, city,country;
+
+    CircleImageView profileImage;
+    TextView Username,address;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +35,46 @@ public class ViewFriendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_friend);
         String userID=getIntent().getStringExtra("userKey");
         Toast.makeText(this,""+userID,Toast.LENGTH_SHORT).show();
+
+        mUserRef= FirebaseDatabase.getInstance().getReference().child("User").child(userID);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+        profileImage=findViewById(R.id.profileImage);
+        Username=findViewById(R.id.username4);
+        address=findViewById(R.id.address);
+
+        LoadUser();
+    }
+
+    private void LoadUser() {
+
+        mUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists())
+                {
+                    profileImageUrl=snapshot.child("profileImage").getValue().toString();
+                    username=snapshot.child("username").getValue().toString();
+                    city=snapshot.child("city").getValue().toString();
+                    country=snapshot.child("country").getValue().toString();
+
+                    Picasso.get().load(profileImageUrl).into(profileImage);
+                    Username.setText(username);
+                    address.setText(city+","+country);
+                }
+                else
+                {
+                    Toast.makeText(ViewFriendActivity.this, "Data not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(ViewFriendActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
